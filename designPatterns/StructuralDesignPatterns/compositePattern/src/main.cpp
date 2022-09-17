6ce2e199fc333c4a3f7776fe50c6c571faa3e845
Nehil Danış <nehild@outlook.com>
2022-09-17 21:33:20 +0200

    ADDED: new example with composite pattern

--- designPatterns/StructuralDesignPatterns/compositePattern/src/main.cpp
+++ designPatterns/StructuralDesignPatterns/compositePattern/src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <algorithm>
 
 struct GraphicObject {
     virtual void draw() = 0;
@@ -33,6 +34,49 @@ struct Group : GraphicObject {
     }
 };
 
+/**
+ * In case a class has many differend fields, it s good to use coposite pattern.
+ * Because if a new field needs to be added the other funtions do not need to be changed.
+ * For example, if there are functions which return sum or the average of all the fields, 
+ * these functions need to be updated when a new field is added.
+ */
+
+class Creature {
+    enum Abilities {strength, agility, intelligence, count};
+    std::array<int, count> abilities;
+
+    public:
+        int get_strength() const { return abilities[strength]; }
+        void set_strength(int value) { abilities[strength] = value; }
+
+        int get_agility() const { return abilities[agility]; }
+        void set_agility(int value) { abilities[agility] = value; }
+
+        int get_intelligence() const { return abilities[intelligence]; }
+        void set_intelligence(int value) { abilities[intelligence] = value; }
+
+        int sum() const { 
+            size_t sum = 0;
+            for(auto const &ability: this->abilities) {
+                sum += ability;
+            }
+            return sum;
+        }
+        double avg() const {
+            return sum() / count;
+        }
+
+        int max() const {
+            return std::max(std::max(abilities[strength], abilities[agility]), abilities[intelligence]);
+        }
+
+        friend std::ostream & operator<<(std::ostream &os, const Creature & c) {
+            os << "Sum: " << c.sum() << " Average: " << c.avg() << " Max: " << c.max() << std::endl;
+            return os;
+        }
+};
+
+
 int main() {
     Group group{"Root"};
     auto subGroup = std::make_unique<Group>(std::string("Flat Drawing"));
@@ -40,9 +84,15 @@ int main() {
     group.objects.push_back(std::move(std::make_unique<Circle>(c1)));
     subGroup->objects.push_back(std::move(std::make_unique<Circle>(c2)));
     group.objects.push_back(std::move(subGroup));
-
     group.draw();
 
+    /* New Example */
+
+    Creature orc;
+    orc.set_strength(10);
+    orc.set_agility(7);
+    orc.set_intelligence(3);
+    std::cout << orc;
 
     return 0;
 }
